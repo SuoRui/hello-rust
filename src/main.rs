7237322a353009a4dfcc6f86f552513632c8b642
suorui <suorui.cn@live.cn>
2022-08-06 00:19:14 +0800

    hash map

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 fn main() {
-    for c in "你好".chars() {
-        println!("{}", c);
+    use std::collections::HashMap;
+
+    let text = "hello world wonderful world";
+
+    let mut map = HashMap::new();
+    for word in text.split_whitespace() {
+        let count = map.entry(word).or_insert(0);
+        *count += 1;
     }
-    println!("======");
-    for c in "你好".bytes() {
-        println!("{}", c);
-    }
+
+    println!("{:?}", map);
 }
