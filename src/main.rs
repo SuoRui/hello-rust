a305f68a00c7fcbc50afb0f9c234398db78080f2
suorui <suorui.cn@live.cn>
2022-08-06 13:03:11 +0800

    泛型

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,22 @@
-fn main() {
-    use std::collections::HashMap;
+fn largest(list: &[i32]) -> i32 {
+    let mut largest = list[0];
 
-    let text = "hello world wonderful world";
-
-    let mut map = HashMap::new();
-    for word in text.split_whitespace() {
-        let count = map.entry(word).or_insert(0);
-        *count += 1;
+    for &item in list {
+        if item > largest {
+            largest = item;
+        }
     }
 
-    println!("{:?}", map);
+    return largest;
+}
+
+fn main() {
+    let num_list = vec![1, 2, 34, 5, 6];
+    let result = largest(&num_list);
+
+    println!("The largest number is {}", result);
+    let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
+
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
 }
